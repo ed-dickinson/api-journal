ee86a348eed4ab3cb7a6405abffdbab1a8a29a7a
ed-dickinson <edwardejd@gmail.com>
2021-08-21 13:30:58 +0100

    fixed fetch doubling

--- frontend/pages.js
+++ frontend/pages.js
@@ -1,11 +1,11 @@
-const directory = 'http://localhost:3000/test/';
+const directory = 'http://localhost:3000/test/'; // local test directory
 
-
+// this is for the front page
 async function recentTitles(directory) {
   let path = 'issues';
   const response = await fetch(directory + path, {mode: 'cors'});
   const recents = await response.json();
-  // console.log(recents);
+
   const recentPosts = document.querySelector('.recent-posts');
   recents.forEach(recent => {
     recentPosts.innerHTML += recent.title;
@@ -24,12 +24,10 @@ const pagesCont = document.querySelector('.inner-pages');
 let noMorePages = false;
 
 
-
 async function getIssue(directory, issue) {
   let path = 'issues/' + issue;
   const response = await fetch(directory + path, {mode: 'cors'});
   const result = await response.json();
-  console.log(result);
   return result;
 }
 
@@ -39,21 +37,19 @@ async function putIssue(directory, issue, page, rL) {
     let path = 'issues/' + issue;
     const response = await fetch(directory + path, {mode: 'cors'});
     const result = await response.json();
-    console.log(result);
     page.title.innerHTML = '<span><span class="title-emoji">' + result.emoji + '</span> ' + result.title + ' <span class="title-emoji">' + result.emoji + '</span></span>';
 
-    console.log(page.title.offsetWidth + '/' + page.title.children[0].offsetWidth);
     page.date.innerHTML = result.date;
     page.body.innerHTML = '<big>' + result.content.substr(0,1) + '</big>' + result.content.substr(1);
     pagesArray.push(page.dom);
     page.title.children[0].style.fontSize = (page.title.offsetWidth / page.title.children[0].offsetWidth) * page.title.offsetWidth * 0.024 + 'px';
 
   } catch (err) {
-    console.log(err);
+    // console.log(err);
     page.title.innerHTML = '';
     page.title.style.fontSize = '2em';
-    page.body.innerHTML = (rL == 'right') ? '<BR><BR><BR><BR><p class="shrug">🙍</p>'  :
-     '<BR><BR><BR><BR><p class="peace">✌️</p>';
+    page.body.innerHTML = (rL == 'right') ? '<p class="shrug">🙍</p>'  :
+     '<p class="peace">✌️</p>';
 
     // if no pages load
     if (issue == 1) {
@@ -99,7 +95,7 @@ function setupPage(no){
   page.style.zIndex = 999 - no;
 
   putIssue(directory, no, {title:title,body:body,date:date,dom:page}, rL);
-
+  innerPagesLoaded.push(no);
 }
 
 const frontPage = document.querySelector('.front-page');
@@ -108,6 +104,8 @@ const backPage = document.querySelector('.back-page');
 const backtisPage = document.querySelector('.backtispiece');
 
 let pagesArray = [frontPage, frontisPage];
+
+let innerPagesLoaded = [];
 
 setupPage(1);
 
@@ -119,18 +117,9 @@ function checkClassAndAdd(element, classe){
   element.classList.add(classe);
 }
 function checkClassAndRemove(element, classe){
-  // if (typeof classe == 'string') {
     if (element.classList.contains(classe)){
       element.classList.remove(classe);
     }
-  // } else {
-  //   classe.forEach(clas => {
-  //     if (element.classList.contains(clas)){
-  //       element.classList.remove(clas);
-  //     }
-  //   })
-  // }
-
 }
 
 let pageNo = -1;
@@ -143,35 +132,31 @@ function turnPage(e) {
       journalCont.classList.add('open');
     },500);
   }
+
   pageNo += 2;
-  // console.log(e);
+
   if (noMorePages == true && pageNo > pagesArray.length - 1) {
     pageNo = pagesArray.length - 1;
   }
-  console.log(pageNo);
-  console.log(pagesArray.length);
 
   let leaf = pagesArray[pageNo-1];
   let overleaf = pagesArray[pageNo];
 
   checkClassAndRemove(leaf, 'unturn-page-event-2');
-  // pagesArray[pageNo-1].classList.add('turn-page-event-1');
   checkClassAndAdd(leaf, 'turn-page-event-1');
 
   setTimeout(function(){
     checkClassAndRemove(overleaf, 'unturn-page-event-1');
-    // pagesArray[pageNo].classList.add('turn-page-event-2');
     checkClassAndAdd(overleaf, 'turn-page-event-2');
-    // pagesArray[pageNo].style.zIndex = pageNo-2;
     overleaf.style.zIndex = pagesArray.indexOf(overleaf);
   },500);
 
-  if (!noMorePages) {
+  //includes is to make sure no double loading
+  if ((!noMorePages) && (!innerPagesLoaded.includes(pageNo+1))) {
     setupPage(pageNo+1);//left
-  } if (!noMorePages) {
+  } if ((!noMorePages) && (!innerPagesLoaded.includes(pageNo+2))) {
     setupPage(pageNo+2); //right
   }
-  // console.log(noMorePages);
 
   if (pageNo == pagesArray.length - 1) {
     checkClassAndRemove(journalCont, 'unclose-book-event');
@@ -185,7 +170,7 @@ function turnPage(e) {
 }
 
 function unturnPage(e) {
-  // console.log(e);
+  if (pageNo == -1) {return;}
   if (pageNo == pagesArray.length - 1) {
     checkClassAndRemove(journalCont, 'close-book-event');
     checkClassAndAdd(journalCont, 'unclose-book-event');
@@ -197,18 +182,15 @@ function unturnPage(e) {
 
   pageNo -= 2;
   if (pageNo < -1) {pageNo = -1;}
-  // console.log(pageNo);
   let leaf = pagesArray[pageNo+2];
   let overleaf = pagesArray[pageNo+1];
 
   leaf.classList.add('unturn-page-event-1');
   setTimeout(function(){
     overleaf.classList.add('unturn-page-event-2');
-    // pagesArray[pageNo+1].style.zIndex = pageNo-2;
   },500);
 
   if (pageNo == -1) {
-    // checkClassAndRemove(journalCont, ['open-book-event','unclose-book-event']);
     checkClassAndRemove(journalCont, 'open-book-event');
     checkClassAndRemove(journalCont, 'unclose-book-event');
     checkClassAndAdd(journalCont, 'unopen-book-event');
--- frontend/sizer.js
+++ frontend/sizer.js
@@ -1,6 +1,4 @@
 const title = document.querySelector('.title');
-const titleX = title.offsetWidth;
-// console.log(titleX);
 
 const journalCont = document.querySelector('.journal-container');
 const viewer = document.querySelector('main');
@@ -9,11 +7,9 @@ let height = 0;
 let width = 0;
 if (window.innerWidth >= window.innerHeight) {
   //landscape
-
   journalCont.style.width = (journalCont.offsetHeight * (3/4)) * 2 + 'px';
   if ((window.innerWidth - 40) < (window.innerHeight - 40) * (8/6)) {
     //center vert
-
     journalCont.style.width = '100%';
     height = journalCont.offsetWidth * (6/8);
     journalCont.style.height = height + 'px';
@@ -26,19 +22,12 @@ if (window.innerWidth >= window.innerHeight) {
     width = journalCont.offsetHeight * (8/6);
     journalCont.style.width = width + 'px';
     viewer.style.width = width + 'px';
-    // viewer.style.marginTop = '20px';
     viewer.style.marginLeft = ((window.innerWidth - width) / 2) + 'px';
   }
   viewer.style.fontSize = journalCont.offsetHeight / 35 + 'px';
-  // console.log(journalCont.offsetHeight);
-
+} else {
+  //portrait
 }
-
-// title.children[1].style.fontSize =
-//   (
-//     (title.offsetWidth - title.children[0].offsetHeight)
-//     / title.children[1].offsetWidth
-//   ) + 'em';
 
 title.children[1].style.fontSize = (title.offsetWidth / title.children[1].offsetWidth)  + 'em';
 
